633d10f4b2738fb2275377bbc3d131b083a9eb57
vincent <oaivyftu@gmail.com>
2025-06-01 11:02:30 -0400

    fix: blur

--- components/ui/Img.tsx
+++ components/ui/Img.tsx
@@ -1,29 +1,14 @@
+"use client"
 import Image, {ImageProps} from 'next/image'
-import {getPlaiceholder} from 'plaiceholder'
-import fs from "node:fs/promises";
-import path from 'path'
 
-export default async function Img(props: ImageProps) {
-  let base64Str : string = ""
-  try {
-    // Chuyển đổi đường dẫn tương đối thành đường dẫn tuyệt đối
-    const publicDir = path.join(process.cwd(), 'public')
-    // Lấy đường dẫn tương đối từ props.src (bỏ dấu / đầu tiên nếu có)
-    const imagePath = (props.src as string).replace(/^\//, '')
-    // Kết hợp thành đường dẫn đầy đủ
-    const fullPath = path.join(publicDir, imagePath)
-
-    const file = await fs.readFile(fullPath);
-    const { base64 } = await getPlaiceholder(file);
-    base64Str = base64
-  } catch (err) {
-    console.error('Error loading image:', err)
-  }
+export default function Img(props: ImageProps) {
+  const theme = localStorage.getItem('theme')
+  const blurDataURL = theme === "light" ? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=" : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII="
   return (
     <Image
       loading="lazy"
       placeholder="blur"
-      blurDataURL={base64Str}
+      blurDataURL={blurDataURL}
       {...props}
     />
   )
